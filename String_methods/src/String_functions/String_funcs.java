76cd72b95d3c59689a34d1fe3af7030ff26cf549
ravikanth995 <ravikanthchauhan95@gmail.com>
2024-02-01 18:10:38 +0530

    Java String

---
+++ String_methods/src/String_functions/String_funcs.java
@@ -0,0 +1,44 @@
+package String_functions;
+
+public class String_funcs {
+
+	public static void main(String[] args) {
+//	String ar = "Ravikanth";
+//	String ar1 = "ravikanth";
+//	System.out.println(ar.equalsIgnoreCase(ar1));
+		
+		//toUpperCase
+//		String ar = "ravikanth";
+//		System.out.println(ar.toUpperCase()); 
+		
+//		String ar1  ="RAVIKANTH";
+//		System.out.println(ar1.toLowerCase());
+		
+		//split()
+//		String ar = "ravikanthchauhan95@gmail.com";
+//		String a[] = ar.split("@");
+//		System.out.println(a[0]);
+//		System.out.println(a[1]);
+
+//		String ar = "Ravikanthchauhan@gmail.com";
+//		String a[]  = ar.split("@");
+//		System.out.println(a[0]);
+//		System.out.println(a[1]); 
+		
+//		String ar = "Ravikanthchauhan95@gmail.com";
+//		String a[] = ar.split("@");
+//		System.out.println(a[0]);
+//		System.out.println(a[1]);
+		
+//		String s = "Ravikanthchauhan@gmail.com";
+//		String a[] = s.split("@");
+//		System.out.println(a[0]);
+//		System.out.println(a[1]);
+		
+		String r = "Ravikanthchuahan95@gmail.com";
+		String a[] = r.split("@");
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+	}
+
+}
