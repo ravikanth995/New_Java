091f5b6f91d0a6a83a5dc25bb9cc5152e269a2aa
ravikanth995 <ravikanthchauhan95@gmail.com>
2024-02-01 13:24:34 +0530

    Java methods

---
+++ String_methods/src/String_functions/string_practice.java
@@ -0,0 +1,106 @@
+package String_functions;
+
+public class string_practice {
+
+	public static void main(String[] args) {
+//		String s = "welcome";
+//		//Length
+//		
+//		System.out.println("Length of the String is :"+s.length());
+
+		
+		//Concat
+//		String s = "Welcome";
+//		String s1 = " to Java";
+//		System.out.println(s.concat(s1));
+		
+//		String s1="Welcome";
+//		String s2 = "To Java";
+//		System.out.println(s1.concat(s2));
+		
+//		String s1 = "Hi World.";
+//		String s2 = "Ravikanth.";
+//		System.out.println(s1.concat(s2));
+		
+//		String s1 = "Ravikanth";
+//		String s2 = "Brother of Shashikanth";
+//		System.out.println(s1.concat(s2));
+		
+//		String s1 = "Hi Dear Brother.  ";
+//		String s2 = "Welcome to Java Tutorial";
+//		System.out.println(s1.concat(s2));
+		
+		//Trimmer
+//		String s = "     Ravi   Kanth";
+//		System.out.println("Length of the Stringn is :"+s.length());
+//		System.out.println("After Trimming or removing spaces are  :"+s.trim().length());
+//		
+		
+//		String s = "                Hi";
+//		System.out.println("length of the String is :"+s.length());
+//		System.out.println(s.trim().length());
+		
+//		String s1 = "HelloWorld             ";
+//		System.out.println("Length is :"+s1.length());
+//		System.out.println("Spaces after removal is :"+s1.trim().length());
+		  
+		//CharAt
+//		String s = "Ravikanth";
+//		System.out.println(s.charAt(0));
+		
+//		String a = "Trail is Me";
+//		System.out.println(a.charAt(0));
+		
+//		String r = "Lakshaman Singh";
+//		System.out.println(r.charAt(4));
+		
+		//Contains
+//		String s = "Ravikanth";
+//		System.out.println(s.contains("Ravi")); 
+		
+//		String s = "KayKayMenon";
+//		System.out.println(s.contains("kay"));
+		
+//		String ar = "Sojugada Mallige";
+//		System.out.println(ar.contains("Mall"));
+		
+//		String a = "Na Vittena tarpanye Manushyah";
+//		System.out.println(a.contains("Na"));
+		
+		//Equals
+		
+//	String	s1 = "Ravikanth";
+//	String 	s2 = "Ravikanth";
+//	System.out.println(s1==s2);
+//	System.out.println(s1.equals(s2));
+		
+		//replace
+
+//		String a = "Sojugada Sujo Mallige Mannimale Mahadev Ninnna Har Har Mahadev";
+//		System.out.println(a.replace("Mahadev", "Har Har Namah"));
+		
+//		String a = "Namah Shivaya.. Om Namah Shivaya.. Shioham Shivoham";
+//		System.out.println(a.replace("Namah", "OM"));
+		
+//		String a  = "Om Namah Shivaya, ChandraChooda ShivaShankar Parvati";
+//		System.out.println(a.replace("Parvati", "Shivashakti"));
+		
+//		String a = "I love Hinduism and i Love Buddha and also Upanishads.";
+//		System.out.println(a.replace("I", "We"));
+		
+//		String a = "I Love Hinduism and Buddha, also Shivaye";
+//		System.out.println(a.replace("Shivaye", "Om Namah Shivaya"));
+		
+		//Substring
+//		String a = "Ravikanth";
+//		System.out.println(a.substring(0, 6));
+		
+//		String a = "Shashi Kanth";
+//		System.out.println(a.substring(0, 9));
+		
+		
+		
+		
+	}   
+
+}
